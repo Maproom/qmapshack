37cdf24c83374a0d80042cfa5770632effbeac3d
kiozen <kiozen@localhost>
2014-07-08 21:17:42 +0200

    Add missing files

---
+++ src/CAbout.cpp
@@ -0,0 +1,41 @@
+/**********************************************************************************************
+    Copyright (C) 2014 Oliver Eichler oliver.eichler@gmx.de
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+**********************************************************************************************/
+
+#include "CAbout.h"
+#include "version.h"
+
+#include <gdal.h>
+#include <proj_api.h>
+#include <QtWidgets>
+
+CAbout::CAbout(QWidget *parent)
+    : QDialog(parent)
+{
+    setupUi(this);
+
+    labelVersion->setText(VER_STR);
+    labelQtVersion->setText(qVersion());
+    labelGDALVersion->setText(GDALVersionInfo("--version"));
+    labelProj4Version->setText(QString::number(PJ_VERSION));
+}
+
+CAbout::~CAbout()
+{
+
+}
+

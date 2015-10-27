a17e42edad467efb15d1e951100da54956a30092
Christian Eichler <code@christian-eichler.de>
2015-10-27 16:33:27 +0100

    Added missing documentation for QTinySpinBox

--- src/widgets/QTinySpinBox.h
+++ src/widgets/QTinySpinBox.h
@@ -1,3 +1,30 @@
+/**********************************************************************************************
+    Copyright (C) 2015 Christian Eichler code@christian-eichler.de
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+**********************************************************************************************/
+
+/** @brief A QWidget derived from QSpinBox, aiming to be tiny with regard to spaced consumed 
+           on screen.
+
+    By default, a readonly QTinySpinBox looks like an ordinary label, whereas a non-readonly
+    QTinySpinBox contains blue, underlined text. As soon as the non-readonly widget receives
+    the focus, the color is changed to black, the underline disappears and the modifiable text
+    is selected.
+ */
+
 #ifndef QTINYSPINBOX_H
 #define QTINYSPINBOX_H
 
@@ -63,6 +90,8 @@ public:
     QTinySpinBox(QWidget * parent = 0)
         : QSpinBox(parent)
     {
+        // initialization has to be done deferred,
+        // as the correct palette is set after construction
         initialized = false;
     }
 

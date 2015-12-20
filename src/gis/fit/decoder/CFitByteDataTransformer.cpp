4469a1bfc7a75fb0dfef20bf7154198be7f7abd8
Ivo <Ivo@ivos-macbook-pro-2.local>
2015-12-20 08:25:39 +0100

    bugfixes (test for invalid value) and code cleanup

---
+++ src/gis/fit/decoder/CFitByteDataTransformer.cpp
@@ -0,0 +1,133 @@
+/**********************************************************************************************
+ Copyright (C) 2014 Oliver Eichler oliver.eichler@gmx.de
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ **********************************************************************************************/
+
+#include "gis/fit/decoder/CFitByteDataTransformer.h"
+
+
+unsigned int CFitByteDataTransformer::getUIntValue(CFitBaseType* baseType, uint8_t* rawData)
+{
+    switch(baseType->nr()) {
+        case TypeUint8:
+        case TypeUint8z:
+        case TypeEnum:
+            return getUint8(rawData);
+        case TypeUint16:
+        case TypeUint16z:
+            return getUint16(rawData);
+        case TypeUint32:
+        case TypeUint32z:
+            return getUint32(rawData);
+        default:
+            return 0;
+    }
+}
+
+int CFitByteDataTransformer::getIntValue(CFitBaseType* baseType, uint8_t* rawData)
+{
+    switch(baseType->nr()) {
+        case TypeSint8:
+            return getSint8(rawData);
+        case TypeSint16:
+            return getSint16(rawData);
+        case TypeSint32:
+            return getSint32(rawData);
+        default:
+            return 0;
+    }
+}
+
+double CFitByteDataTransformer::getDoubleValue(CFitBaseType* baseType, uint8_t* rawData)
+{
+    switch(baseType->nr()) {
+        case TypeFloat32:
+            return getFloat32(rawData);
+        case TypeFloat64:
+            return getFloat64(rawData);
+        default:
+            return 0;
+    }
+}
+
+uint8_t CFitByteDataTransformer::getUint8(uint8_t* rawData)
+{
+    return (uint8_t) rawData[0];
+}
+
+uint16_t CFitByteDataTransformer::getUint16(uint8_t* rawData)
+{
+    return ((uint16_t)rawData[1] << 8) | (uint16_t)rawData[0];
+}
+
+uint32_t CFitByteDataTransformer::getUint32(uint8_t* rawData)
+{
+    return (((uint32_t)rawData[3] << 24) | ((uint32_t)rawData[2] << 16) | ((uint32_t)rawData[1] << 8) | (uint32_t)rawData[0]);
+}
+
+int8_t CFitByteDataTransformer::getSint8(uint8_t* rawData)
+{
+    return (int8_t) rawData[0];
+}
+
+int16_t CFitByteDataTransformer::getSint16(uint8_t* rawData)
+{
+    return ((int16_t)rawData[1] << 8) | (int16_t)rawData[0];
+}
+
+int32_t CFitByteDataTransformer::getSint32(uint8_t* rawData)
+{
+    return (((int32_t)rawData[3] << 24) | ((int32_t)rawData[2] << 16) | ((int32_t)rawData[1] << 8) | (int32_t)rawData[0]);
+}
+
+float CFitByteDataTransformer::getFloat32(uint8_t* rawData)
+{
+    int32_t fValue = (int32_t) (((int32_t)rawData[3] << 24) | ((int32_t)rawData[2] << 16) | ((int32_t)rawData[1] << 8) | rawData[0]);
+    float value;
+    memcpy(&value, &fValue, sizeof(float));
+    return value;
+}
+
+double CFitByteDataTransformer::getFloat64(uint8_t* rawData)
+{
+    unsigned long long dValue = ((unsigned long long) rawData[7] << 56) | ((unsigned long long) rawData[6] << 48)
+                                | ((unsigned long long) rawData[5] << 40) | ((unsigned long long) rawData[4] << 32)
+                                | ((unsigned long long) rawData[3] << 24) | ((unsigned long long) rawData[2] << 16)
+                                | ((unsigned long long) rawData[1] << 8) | rawData[0];
+    double value;
+    memcpy(&value, &dValue, sizeof(double));
+    return value;
+}
+
+QString CFitByteDataTransformer::getString(uint8_t* rawData, uint8_t length)
+{
+    QString str;
+    uint8_t i = 0;
+    while(rawData[i] != 0 )
+    {
+        str = str.append(rawData[i]);
+        i++;
+        if(length > 0 && i > length)
+            break;
+    }
+    return str;
+}
+
+uint8_t* CFitByteDataTransformer::getBytes(uint8_t* rawData)
+{
+    // TODO
+    return rawData;
+}

bdba572d94f6c0c00ee1f74aa4d6c15ef753cb61
martin-13 <martin.schmidt13@gmx.de>
2016-12-16 05:06:32 +0100

    Makes DBUS optional

--- src/device/IDevice.cpp
+++ src/device/IDevice.cpp
@@ -22,7 +22,7 @@
 #include "gis/prj/IGisProject.h"
 #include "helpers/CSelectCopyAction.h"
 
-#if defined(Q_OS_LINUX) || defined(Q_OS_FREEBSD)
+#if defined(Q_OS_LINUX) && defined(HAVE_DBUS) || defined(Q_OS_FREEBSD)
 #include <QtDBus>
 #endif
 
@@ -52,7 +52,7 @@ IDevice::~IDevice()
 
 void IDevice::mount(const QString& path)
 {
-#if defined(Q_OS_LINUX) || defined(Q_OS_FREEBSD)
+#if defined(Q_OS_LINUX) && defined(HAVE_DBUS) || defined(Q_OS_FREEBSD)
     QDBusMessage message = QDBusMessage::createMethodCall("org.freedesktop.UDisks2",path,"org.freedesktop.UDisks2.Filesystem","Mount");
     QVariantMap args;
     args.insert("options", "sync");
@@ -68,7 +68,7 @@ void IDevice::mount(const QString& path)
 
 void IDevice::umount(const QString &path)
 {
-#if defined(Q_OS_LINUX) || defined(Q_OS_FREEBSD)
+#if defined(Q_OS_LINUX) && defined(HAVE_DBUS) || defined(Q_OS_FREEBSD)
     QDBusMessage message = QDBusMessage::createMethodCall("org.freedesktop.UDisks2",path,"org.freedesktop.UDisks2.Filesystem","Unmount");
     QVariantMap args;
     message << args;

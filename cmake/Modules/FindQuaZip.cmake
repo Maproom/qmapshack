d6b91d9218f5bf7062d080669e9e2431ac532912
kiozen <kiozen@localhost>
2017-03-18 13:58:54 +0100

    Fix the cmake find script for QuaZip5

--- cmake/Modules/FindQuaZip.cmake
+++ cmake/Modules/FindQuaZip.cmake
@@ -1,43 +1,81 @@
-# QUAZIP_FOUND               - QuaZip library was found
-# QUAZIP_INCLUDE_DIR         - Path to QuaZip include dir
-# QUAZIP_INCLUDE_DIRS        - Path to QuaZip and zlib include dir (combined from QUAZIP_INCLUDE_DIR + ZLIB_INCLUDE_DIR)
-# QUAZIP_LIBRARIES           - List of QuaZip libraries
-# QUAZIP_ZLIB_INCLUDE_DIR    - The include dir of zlib headers
+# - Try to find QUAZIP
+# Once done this will define
+#
+#  QUAZIP_FOUND - system has QUAZIP
+#  QUAZIP_INCLUDE_DIRS - the QUAZIP include directory
+#  QUAZIP_LIBRARIES - Link these to use QUAZIP
+#  QUAZIP_DEFINITIONS - Compiler switches required for using QUAZIP
+#
+#  Redistribution and use is allowed according to the terms of the New
+#  BSD license.
+#  For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+#
 
 
-IF (QUAZIP_INCLUDE_DIRS AND QUAZIP_LIBRARIES)
-	# in cache already
-	SET(QUAZIP_FOUND TRUE)
-ELSE (QUAZIP_INCLUDE_DIRS AND QUAZIP_LIBRARIES)
-    IF (Qt5Core_FOUND)
-        set(QUAZIP_LIB_VERSION_SUFFIX 5)
-    ENDIF()
-	IF (WIN32)
-		FIND_PATH(QUAZIP_LIBRARY_DIR
-			WIN32_DEBUG_POSTFIX d
-            NAMES libquazip${QUAZIP_LIB_VERSION_SUFFIX}.dll
-			HINTS "C:/Programme/" "C:/Program Files"
-			PATH_SUFFIXES QuaZip/lib
-		)
-        FIND_LIBRARY(QUAZIP_LIBRARIES NAMES libquazip${QUAZIP_LIB_VERSION_SUFFIX}.dll HINTS ${QUAZIP_LIBRARY_DIR})
-		FIND_PATH(QUAZIP_INCLUDE_DIR NAMES quazip.h HINTS ${QUAZIP_LIBRARY_DIR}/../ PATH_SUFFIXES include/quazip)
-		FIND_PATH(QUAZIP_ZLIB_INCLUDE_DIR NAMES zlib.h)
-	ELSE(WIN32)
-		FIND_PACKAGE(PkgConfig)
-#     pkg_check_modules(PC_QCA2 QUIET qca2)
-		pkg_check_modules(PC_QUAZIP quazip)
-		FIND_LIBRARY(QUAZIP_LIBRARIES
-			WIN32_DEBUG_POSTFIX d
-            NAMES quazip${QUAZIP_LIB_VERSION_SUFFIX}
-			HINTS /usr/lib /usr/lib64
-		)
-		FIND_PATH(QUAZIP_INCLUDE_DIR quazip.h
-			HINTS /usr/include /usr/local/include
-			PATH_SUFFIXES quazip${QUAZIP_LIB_VERSION_SUFFIX}
-		)
-		FIND_PATH(QUAZIP_ZLIB_INCLUDE_DIR zlib.h HINTS /usr/include /usr/local/include)
-	ENDIF (WIN32)
-	INCLUDE(FindPackageHandleStandardArgs)
-	SET(QUAZIP_INCLUDE_DIRS ${QUAZIP_INCLUDE_DIR} ${QUAZIP_ZLIB_INCLUDE_DIR})
-	find_package_handle_standard_args(QUAZIP DEFAULT_MSG  QUAZIP_LIBRARIES QUAZIP_INCLUDE_DIR QUAZIP_ZLIB_INCLUDE_DIR QUAZIP_INCLUDE_DIRS)
-ENDIF (QUAZIP_INCLUDE_DIRS AND QUAZIP_LIBRARIES)
+if (QUAZIP_LIBRARIES AND QUAZIP_INCLUDE_DIRS)
+    # in cache already
+    set(QUAZIP_FOUND TRUE)
+else (QUAZIP_LIBRARIES AND QUAZIP_INCLUDE_DIRS)
+
+    find_path(QUAZIP_INCLUDE_DIR
+        NAMES
+            quazip.h
+        PATHS
+            /usr/include
+            /usr/local/include
+            /opt/local/include
+            /sw/include
+            /usr/include/quazip5
+            /usr/local/include/quazip5
+            /opt/local/include/quazip5
+            /sw/include/quazip5
+            ${CMAKE_INSTALL_PREFIX}/include
+            ${QUAZIP_DEV_PATH}/include/
+    )
+    mark_as_advanced(QUAZIP_INCLUDE_DIR)
+
+    find_library(LIBQUAZIP_LIBRARY
+        NAMES
+            quazip5
+        PATHS
+            /usr/lib
+            /usr/local/lib
+            /opt/local/lib
+            /sw/lib
+            ${CMAKE_INSTALL_PREFIX}/include
+            ${QUAZIP_DEV_PATH}/lib
+    )
+    mark_as_advanced(LIBQUAZIP_LIBRARY)
+
+    if (LIBQUAZIP_LIBRARY)
+        set(LIBQUAZIP_FOUND TRUE)
+    endif (LIBQUAZIP_LIBRARY)
+
+    set(QUAZIP_INCLUDE_DIRS
+        ${QUAZIP_INCLUDE_DIR}
+    )
+
+    if (LIBQUAZIP_FOUND)
+        set(QUAZIP_LIBRARIES ${QUAZIP_LIBRARIES}  ${LIBQUAZIP_LIBRARY})
+    endif (LIBQUAZIP_FOUND)
+
+    if (QUAZIP_INCLUDE_DIRS AND QUAZIP_LIBRARIES)
+        set(QUAZIP_FOUND TRUE)
+    endif (QUAZIP_INCLUDE_DIRS AND QUAZIP_LIBRARIES)
+
+    if (QUAZIP_FOUND)
+        if (NOT QUAZIP_FIND_QUIETLY)
+            message(STATUS "Found QUAZIP: ${QUAZIP_LIBRARIES}")
+            message(STATUS "Found QUAZIP: ${QUAZIP_INCLUDE_DIR}")
+        endif (NOT QUAZIP_FIND_QUIETLY)
+    else (QUAZIP_FOUND)
+        if (QUAZIP_FIND_REQUIRED)
+            message(FATAL_ERROR "Could not find QUAZIP")
+        endif (QUAZIP_FIND_REQUIRED)
+    endif (QUAZIP_FOUND)
+
+    # show the QUAZIP_INCLUDE_DIRS and QUAZIP_LIBRARIES variables only in the advanced view
+    mark_as_advanced(QUAZIP_INCLUDE_DIRS QUAZIP_LIBRARIES)
+
+endif (QUAZIP_LIBRARIES AND QUAZIP_INCLUDE_DIRS)
+

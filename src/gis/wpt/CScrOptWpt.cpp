fb107803ec45dcc0504a5505e52454e70421e22d
ntruchsess <ntruchsess@localhost>
2017-12-03 11:08:03 +0100

    add copyright notices

--- src/gis/wpt/CScrOptWpt.cpp
+++ src/gis/wpt/CScrOptWpt.cpp
@@ -1,5 +1,6 @@
 /**********************************************************************************************
     Copyright (C) 2014 Oliver Eichler oliver.eichler@gmx.de
+    Copyright (C) 2017 Norbert Truchsess norbert.truchsess@t-online.de
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by

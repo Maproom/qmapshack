69b93d080de2aa588142588c10e67f2191ab5c6c
kiozen <kiozen@localhost>
2018-01-09 19:24:09 +0100

    Restructure cmake files

--- cmake/Modules/DefineCompilerFlags.cmake
+++ cmake/Modules/DefineCompilerFlags.cmake
@@ -11,3 +11,11 @@ if(UNIX AND NOT WIN32)
     endif(WITH_FPIC)
   endif(CMAKE_SYSTEM_PROCESSOR MATCHES "x86_64")
 endif(UNIX AND NOT WIN32)
+
+# function to test for compile flags
+function(cxx_add_flag_if_supported flag)
+    CHECK_CXX_COMPILER_FLAG(${flag} Flag:${flag})
+    if(Flag:${flag})
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${flag}" PARENT_SCOPE)
+    endif(Flag:${flag})
+endfunction(cxx_add_flag_if_supported)

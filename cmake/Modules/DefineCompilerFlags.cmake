c3fe233751d749d39e6c539520c2851e169ebbda
kiozen <kiozen@localhost>
2018-01-10 09:24:17 +0100

    Cleaning up cmake files
    Add comment and a bit more restructuring.

--- cmake/Modules/DefineCompilerFlags.cmake
+++ cmake/Modules/DefineCompilerFlags.cmake
@@ -14,8 +14,29 @@ endif(UNIX AND NOT WIN32)
 
 # function to test for compile flags
 function(cxx_add_flag_if_supported flag)
-    CHECK_CXX_COMPILER_FLAG(${flag} Flag:${flag})
+    check_cxx_compiler_flag(${flag} Flag:${flag})
     if(Flag:${flag})
         set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${flag}" PARENT_SCOPE)
     endif(Flag:${flag})
 endfunction(cxx_add_flag_if_supported)
+
+
+function(cxx_local_system_optimization)
+    set(flag "-march=native")
+    check_cxx_compiler_flag(${flag} Flag:${flag})
+    if(Flag:${flag})
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${flag}")
+        message(STATUS [=[
+ BUILDING FOR LOCAL SYSTEM ONLY
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ Specifying -DBUILD_FOR_LOCAL_SYSTEM=ON will pass -march=native to the compiler.
+ The generated binary will exhibit higher performance,
+ but will not be portable (e.g., might not work on other CPUs)]=])
+    else(Flag:${flag})
+        message(STATUS [=[
+ Your compiler does not support -march=native.
+ Ignoring -DBUILD_FOR_LOCAL_SYSTEM=ON!]=])
+    endif(Flag:${flag})
+   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -march=native")
+
+endfunction(cxx_local_system_optimization)
--- cmake/Modules/DefineInstallationPaths.cmake
+++ cmake/Modules/DefineInstallationPaths.cmake
@@ -1,6 +1,6 @@
 if (UNIX)
 
-  STRING(TOLOWER ${PROJECT_NAME} _APPLICATION_NAME)
+  STRING(TOLOWER ${PROJECT_NAME} _PROJECT_NAME)
 
   # Suffix for Linux
   SET(LIB_SUFFIX
@@ -24,12 +24,12 @@ if (UNIX)
   # The following are directories where stuff will be installed to
   SET(BIN_INSTALL_DIR
     "${EXEC_INSTALL_PREFIX}/bin"
-    CACHE PATH "The ${_APPLICATION_NAME} binary install dir (default prefix/bin)"
+    CACHE PATH "The ${_PROJECT_NAME} binary install dir (default prefix/bin)"
     FORCE
   )
   SET(SBIN_INSTALL_DIR
     "${EXEC_INSTALL_PREFIX}/sbin"
-    CACHE PATH "The ${_APPLICATION_NAME} sbin install dir (default prefix/sbin)"
+    CACHE PATH "The ${_PROJECT_NAME} sbin install dir (default prefix/sbin)"
     FORCE
   )
   SET(LIB_INSTALL_DIR
@@ -43,8 +43,8 @@ if (UNIX)
     FORCE
   )
   SET(PLUGIN_INSTALL_DIR
-    "${LIB_INSTALL_DIR}/${_APPLICATION_NAME}"
-    CACHE PATH "The subdirectory relative to the install prefix where plugins will be installed (default is prefix/lib/${_APPLICATION_NAME})"
+    "${LIB_INSTALL_DIR}/${_PROJECT_NAME}"
+    CACHE PATH "The subdirectory relative to the install prefix where plugins will be installed (default is prefix/lib/${_PROJECT_NAME})"
     FORCE
   )
   SET(INCLUDE_INSTALL_DIR
@@ -55,7 +55,7 @@ if (UNIX)
 
   SET(DATA_INSTALL_DIR
     "${DATA_INSTALL_PREFIX}"
-    CACHE PATH "The parent directory where applications can install their data (default prefix/share/${_APPLICATION_NAME})"
+    CACHE PATH "The parent directory where applications can install their data (default prefix/share/${_PROJECT_NAME})"
     FORCE
   )
   SET(HTML_INSTALL_DIR
@@ -93,17 +93,17 @@ if (UNIX)
 
   SET(SYSCONF_INSTALL_DIR
     "${EXEC_INSTALL_PREFIX}/etc"
-    CACHE PATH "The ${_APPLICATION_NAME} sysconfig install dir (default prefix/etc)"
+    CACHE PATH "The ${_PROJECT_NAME} sysconfig install dir (default prefix/etc)"
     FORCE
   )
   SET(MAN_INSTALL_DIR
     "${SHARE_INSTALL_PREFIX}/man"
-    CACHE PATH "The ${_APPLICATION_NAME} man install dir (default prefix/man)"
+    CACHE PATH "The ${_PROJECT_NAME} man install dir (default prefix/man)"
     FORCE
   )
   SET(INFO_INSTALL_DIR
     "${SHARE_INSTALL_PREFIX}/info"
-    CACHE PATH "The ${_APPLICATION_NAME} info install dir (default prefix/info)"
+    CACHE PATH "The ${_PROJECT_NAME} info install dir (default prefix/info)"
     FORCE
   )
 endif (UNIX)

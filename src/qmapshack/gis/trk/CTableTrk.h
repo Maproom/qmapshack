ac46e9d03d290f76c56f90ec8dd88c26e4ab7fcc
Karl Karsten <mail@karl-karsten.de>
2018-03-30 22:42:13 +0200

    Minor code changes to sync with default.

--- src/qmapshack/gis/trk/CTableTrk.h
+++ src/qmapshack/gis/trk/CTableTrk.h
@@ -36,6 +36,7 @@ public:
     void setMouseRangeFocus(const CTrackData::trkpt_t * pt1, const CTrackData::trkpt_t * pt2) override {}
     void setMouseClickFocus(const CTrackData::trkpt_t * pt) override {}
 
+
     enum columns_t
     {
         eColNum

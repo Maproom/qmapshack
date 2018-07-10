ab61a56e8efd0542505ed919f99fed039a7f03db
kiozen <kiozen@localhost>
2018-07-10 08:08:13 +0200

    Fix compiler error for older GDAL versions

--- src/qmapshack/dem/CDemVRT.cpp
+++ src/qmapshack/dem/CDemVRT.cpp
@@ -75,8 +75,8 @@ CDemVRT::CDemVRT(const QString &filename, CDemDraw *parent)
         strncpy(str, dataset->GetProjectionRef(), sizeof(str) - 1);
     }
     OGRSpatialReference oSRS;
-    const char *wkt = str;
-    oSRS.importFromWkt(wkt);
+    char *wkt = str;
+    oSRS.importFromWkt(&wkt);
 
     char *proj4 = nullptr;
     oSRS.exportToProj4(&proj4);

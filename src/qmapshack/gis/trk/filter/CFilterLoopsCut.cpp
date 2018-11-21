008c513e03a616325af4cf85f9d1d15d912607d8
zero <zero@cms123.fr>
2018-11-21 18:44:29 +0100

    Added help button and save to SETTINGS

--- src/qmapshack/gis/trk/filter/CFilterLoopsCut.cpp
+++ src/qmapshack/gis/trk/filter/CFilterLoopsCut.cpp
@@ -33,15 +33,16 @@ CFilterLoopsCut::CFilterLoopsCut(CGisItemTrk &trk, QWidget * parent)
     spinBox->setSuffix(IUnit::self().baseunit);
 
     SETTINGS;
-    spinBox->setValue(cfg.value("TrackDetails/Filter/LoopsCut/distance",5).toInt());
+    spinBox->setValue(cfg.value("TrackDetails/Filter/LoopsCut/minLoopLength",10).toInt());
 
     connect(toolApply, &QToolButton::clicked, this, &CFilterLoopsCut::slotApply);
+    connect(help, &QToolButton::clicked, this, &CFilterLoopsCut::showHelp);
 }
 
 CFilterLoopsCut::~CFilterLoopsCut()
 {
     SETTINGS;
-    cfg.setValue("TrackDetails/Filter/DouglasPeuker/distance", spinBox->value());
+    cfg.setValue("TrackDetails/Filter/LoopsCut/minLoopLength", spinBox->value());
 }
 
 void CFilterLoopsCut::slotApply()
@@ -49,4 +50,25 @@ void CFilterLoopsCut::slotApply()
     CCanvas::setOverrideCursor(Qt::WaitCursor, "CFilterLoopsCut");
     trk.filterLoopsCut(spinBox->value()/IUnit::self().basefactor);
     CCanvas::restoreOverrideCursor("CFilterLoopsCut");
+}
+
+void CFilterLoopsCut::showHelp()
+{
+    QMessageBox::information(CMainWindow::getBestWidgetForParent(), tr("Help")
+                                   , tr("Sometimes tracks have loops.\n\n"
+                                        "It is often the case for mountain bikes tours where the same fire road is used several "
+                                        "times to go back uphill to an high point. "
+                                        "Several downhill paths start from this high point and the same fire road is used again "
+                                        "and again to go back up. Then the recorded track is made of several loops, and following "
+                                        "a track made of loops on a Garmin device is difficult, as it is impossible to know in what "
+                                        "order loops have to be ridden.\n\n"
+                                        "Then the purpose of this filter is to split the input track into several separate tracks. "
+                                        "The obtained tracks have no loops and are easy to follow on a Garmin device: start with part 1, "
+                                        "then when end of part 1 is reached, switch to part 2, etc..\n\n"
+                                        "This filter detects loops by looking for intersections. A new track is created as soon as an "
+                                        "intersection is detected.\n\n"
+                                        "The only input parameter is minimum loop length: this is to prevent cutting tracks in "
+                                        "tight switchbacks, where recorded tracks can be made of tiny loops. These loops will be ignored "
+                                        "if their lengths are smaller that the given value.")  );
+
 }

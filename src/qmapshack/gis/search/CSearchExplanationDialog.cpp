725b18cef40b3dc5f7315263f20c8cfa08157cd5
pingurus <pingurus@localhost>
2019-07-15 08:52:54 -0700

    Possible fix for @wthaemelts problem of disappearing window

--- src/qmapshack/gis/search/CSearchExplanationDialog.cpp
+++ src/qmapshack/gis/search/CSearchExplanationDialog.cpp
@@ -22,6 +22,7 @@ CSearchExplanationDialog::CSearchExplanationDialog(QWidget *parent)
 {
     setupUi(this);
     setWindowFlag(Qt::Tool, true);
+    setWindowFlag(Qt::WindowStaysOnTopHint, true);
     //connect(buttonBox, &QDialogButtonBox::accepted, this, &CGeoSearchConfigDialog::slotAccepted);
     QString explanation = tr("The underlying syntax of the search is \"Property Comparison Value\", i.e. \"name contains water\", however there are measures in place to make searching more intuitive so you can omit parts in special cases. Just try it out. \nThe searches are always exclusive, thus for a track \"elevation under 500m\" only shows tracks that are completly under 500m meters. ");
     explanation += tr("\nYou can write Dates in the following formats:");

bd6a9dc17e64bd98ee62c202fc05512498bceb82
kkarsten62 <kkarsten62@localhost>
2019-08-07 22:13:46 +0200

    Comments in serialization.cpp removed.

--- src/qmapshack/gis/qms/serialization.cpp
+++ src/qmapshack/gis/qms/serialization.cpp
@@ -568,7 +568,6 @@ QDataStream& CGisItemTrk::operator>>(QDataStream& stream) const
     out << limitsGraph2;
     out << limitsGraph3;
 
-//    out << energyUse;
     out << energyCycling.getEnergyTrkSet();
 
     out << trk.segs;
@@ -648,15 +647,10 @@ QDataStream& CGisItemTrk::operator<<(QDataStream& stream)
 
     if(version > 5)
     {
-//        in >> energyUse;
         CEnergyCycling::energy_set_t set;
         in >> set;
         energyCycling.setEnergyTrkSet(set, false);
     }
-//    else
-//    {
-//        energyUse = NOFLOAT;
-//    }
 
     trk.segs.clear();
     in >> trk.segs;

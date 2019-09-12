763cfc149566325cce9e4690cb7b5f986048f86a
Oliver Eichler <oliver.eichler@dspsolutions.de>
2019-09-12 20:32:06 +0200

    [QMS-3] Add qmt_rgb2pct from former sub-repo

---
+++ src/qmt_rgb2pct/locale/qmt_rgb2pct.ts
@@ -0,0 +1,126 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>CApp</name>
+    <message>
+        <location filename="../CApp.cpp" line="63"/>
+        <source>Failed to open source file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../CApp.cpp" line="68"/>
+        <source>Raster band count of source file must be either 3 or 4.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../CApp.cpp" line="101"/>
+        <source>Calculate optimal color table from source file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../CApp.cpp" line="115"/>
+        <source>Failed to create color table.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../CApp.cpp" line="123"/>
+        <source>Failed to open file with palette.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../CApp.cpp" line="131"/>
+        <source>Palette file does not have a single band with a color table</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../CApp.cpp" line="140"/>
+        <source>The color table must not contain a &quot;no data&quot; value and it&apos;s size must not exceed 255 colors.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../CApp.cpp" line="167"/>
+        <source>Save color table to: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../CApp.cpp" line="203"/>
+        <source>Failed to create target file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../CApp.cpp" line="214"/>
+        <source>Dither source file to target file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../CApp.cpp" line="225"/>
+        <source>Failed to dither file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../CApp.cpp" line="240"/>
+        <source>Apply alpha channel as no data value to target file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../CApp.cpp" line="247"/>
+        <source>Failed to read from alpha channel.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../CApp.cpp" line="253"/>
+        <source>Failed to read from target file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../CApp.cpp" line="267"/>
+        <source>Failed to write to target file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.cpp" line="96"/>
+        <source>
+Convert a map file with RGBA color coding to a color palette coding.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="99"/>
+        <source>Source file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="100"/>
+        <source>Target file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="104"/>
+        <source>Number of colors. (default: 255)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="107"/>
+        <source>Input palette file for color table (*.vrt)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="110"/>
+        <source>Save color table to palette file (*.vrt)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="120"/>
+        <source>There must be a source and destination file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="143"/>
+        <source>--ncolors must be an integer value less than 256</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>

191d4d8fc15e50eb6b73ab66923e4ecfe4fdc602
Dr. Johannes Zellner <johannes@zellner.org>
2019-10-17 20:41:59 +0200

    [QMS-22] Added units "Aviation nm/feet"
    in aviation nautical miles ar used for distances
    and feet for altitudes / heights.
    
    This adds a check button "Aviation (nm, feet)"
    to units which sets nautical miles and knots
    for horizontal distances and speed and
    feet for vertical distances.

---
+++ src/qmapshack/units/CUnitAviation.cpp
@@ -0,0 +1,127 @@
+/**********************************************************************************************
+    Copyright (C) 2008 Oliver Eichler oliver.eichler@gmx.de
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111 USA
+
+**********************************************************************************************/
+
+#include "units/CUnitAviation.h"
+
+CUnitAviation::CUnitAviation(QObject * parent)
+    : IUnit(eTypeAviation, "ft", footPerMeter, "kn", meterPerSecToKnots, parent)
+{
+}
+
+
+void CUnitAviation::meter2elevation(qreal meter, QString& val, QString& unit) const /* override */
+{
+    if(meter == NOFLOAT)
+    {
+        val  = "-";
+        unit.clear();
+    }
+    else
+    {
+        val.sprintf("%1.0f", meter * footPerMeter);
+        unit = "ft";
+    }
+}
+
+void CUnitAviation::meter2elevation(qreal meter, qreal& val, QString& unit) const /* override */
+{
+    if(meter == NOFLOAT)
+    {
+        val  = NOFLOAT;
+        unit.clear();
+    }
+    else
+    {
+        val =  meter * footPerMeter;
+        unit = "ft";
+    }
+}
+
+void CUnitAviation::meter2distance(qreal meter, QString& val, QString& unit) const /* override */
+{
+    if(meter == NOFLOAT)
+    {
+        val  = "-";
+        unit.clear();
+    }
+    else if(meter < 1852)
+    {
+        val.sprintf("%g", meter * nauticalMilePerMeter);
+        unit = "nm";
+    }
+    else
+    {
+        val.sprintf("%1.1f", meter * nauticalMilePerMeter);
+        unit = "nm";
+    }
+}
+
+void CUnitAviation::meter2distance(qreal meter, qreal& val, QString& unit) const /* override */
+{
+    if(meter == NOFLOAT)
+    {
+        val  = NOFLOAT;
+        unit.clear();
+    }
+    else
+    {
+        val = meter * nauticalMilePerMeter;
+        unit = "nm";
+    }
+}
+
+void CUnitAviation::meter2area(qreal meter, QString& val, QString& unit) const /* override */
+{
+    if(meter == NOFLOAT)
+    {
+        val  = "-";
+        unit.clear();
+    }
+    else
+    {
+        val.sprintf("%1.2f", meter / (1/nauticalMilePerMeter * 1/nauticalMilePerMeter));
+        unit = "nm²";
+    }
+}
+
+void CUnitAviation::meter2area(qreal meter, qreal& val, QString& unit) const /* override */
+{
+    if(meter == NOFLOAT)
+    {
+        val  = NOFLOAT;
+        unit.clear();
+    }
+    else
+    {
+        val= meter / (1/nauticalMilePerMeter * 1/nauticalMilePerMeter);
+        unit = "nm²";
+    }
+}
+
+qreal CUnitAviation::elevation2meter(const QString& val) const /* override */
+{
+    return val.toDouble() / footPerMeter;
+}
+
+void CUnitAviation::meter2unit(qreal meter, qreal& scale, QString&  unit) const
+{
+    scale = nauticalMilePerMeter;
+    unit  = "nm";
+}
+
---
+++ src/qmapshack/units/CUnitAviation.h
@@ -0,0 +1,43 @@
+/**********************************************************************************************
+    Copyright (C) 2008 Oliver Eichler oliver.eichler@gmx.de
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111 USA
+
+**********************************************************************************************/
+#ifndef CUNITAVIATION_H
+#define CUNITAVIATION_H
+
+#include "IUnit.h"
+
+class CUnitAviation : public IUnit
+{
+public:
+    static constexpr qreal footPerMeter = 3.28084;
+    static constexpr qreal nauticalMilePerMeter = 1. / 1852;
+    static constexpr qreal meterPerSecToKnots = 1.94361780;
+
+    CUnitAviation(QObject * parent);
+    virtual ~CUnitAviation() = default;
+
+    void meter2elevation(qreal meter, QString& val, QString& unit) const override;
+    void meter2elevation(qreal meter, qreal& val, QString& unit) const override;
+    void meter2distance(qreal meter, QString& val, QString& unit) const override;
+    void meter2distance(qreal meter, qreal& val, QString& unit) const override;
+    void meter2area(qreal meter, QString& val, QString& unit) const override;
+    void meter2area(qreal meter, qreal& val, QString& unit) const override;
+    qreal elevation2meter(const QString& val) const override;
+    void meter2unit(qreal meter, qreal& scale, QString&  unit) const override;
+};
+#endif //CUNITAVIATION_H

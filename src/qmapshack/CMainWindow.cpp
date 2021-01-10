73eb8ad041c25a3128e904ed4b10f0a7f410a9b7
kiozen <oliver.eichler@gmx.de>
2021-01-10 11:26:39 +0100

    [QMS-297] Add GUI framework for POI databases
    * Added a new docker widget with all necessary hooks for propper GUI
      configuration.
    * Added basic list for POI collections.
    * Added basic implementation for the Mapsforge POI files
    * Added basic configuration

--- src/qmapshack/CMainWindow.cpp
+++ src/qmapshack/CMainWindow.cpp
@@ -49,6 +49,8 @@
 #include "map/CMapDraw.h"
 #include "map/CMapItem.h"
 #include "map/CMapList.h"
+#include "poi/CPoiDraw.h"
+#include "poi/CPoiList.h"
 #include "print/CScreenshotDialog.h"
 #include "realtime/CRtWorkspace.h"
 #include "setup/IAppSetup.h"
@@ -78,12 +80,13 @@ CMainWindow * CMainWindow::pSelf = nullptr;
 
 QDir CMainWindow::homeDir;
 const QString CMainWindow::mapsPath = "Maps";
+const QString CMainWindow::poisPath = "POI";
 const QString CMainWindow::demPath = "DEM";
 const QString CMainWindow::routinoPath = "Routino";
 const QString CMainWindow::brouterPath = "BRouter";
 const QString CMainWindow::databasePath = "Databases";
 const QString CMainWindow::gpxPath = "GPX";
-const QSet<QString> CMainWindow::paths = {mapsPath, demPath, routinoPath, brouterPath, databasePath, gpxPath};
+const QSet<QString> CMainWindow::paths = {mapsPath, poisPath, demPath, routinoPath, brouterPath, databasePath, gpxPath};
 
 QMutex CMainWindow::mutex(QMutex::NonRecursive);
 
@@ -208,6 +211,7 @@ CMainWindow::CMainWindow()
     connect(actionSetupGrid,             &QAction::triggered,            this,      &CMainWindow::slotSetupGrid);
     connect(actionSetupMapPaths,         &QAction::triggered,            this,      &CMainWindow::slotSetupMapPath);
     connect(actionSetupDEMPaths,         &QAction::triggered,            this,      &CMainWindow::slotSetupDemPath);
+    connect(actionSetupPOIPaths,         &QAction::triggered,            this,      &CMainWindow::slotSetupPoiPath);
     connect(actionSetupMapView,          &QAction::triggered,            this,      &CMainWindow::slotSetupMapView);
     connect(actionSetupTimeZone,         &QAction::triggered,            this,      &CMainWindow::slotSetupTimeZone);
     connect(actionSetupUnits,            &QAction::triggered,            this,      &CMainWindow::slotSetupUnits);
@@ -243,6 +247,7 @@ CMainWindow::CMainWindow()
     cfg.beginGroup("Canvas");
     CMapDraw::loadMapPath(cfg);
     CDemDraw::loadDemPath(cfg);
+    CPoiDraw::loadPoiPath(cfg);
 
     cfg.beginGroup("Views");
     QStringList names = cfg.childGroups();
@@ -304,6 +309,7 @@ CMainWindow::CMainWindow()
 
     docks << dockMaps
           << dockDem
+          << dockPoi
           << dockWorkspace
           << dockDatabase
           << dockRte
@@ -343,6 +349,11 @@ CMainWindow::CMainWindow()
     actionToggleDem->setObjectName("actionToggleDem");
     actionToggleDem->setIcon(QIcon(":/icons/32x32/ToggleDem.png"));
     menuWindow->insertAction(actionSetupToolbar, actionToggleDem);
+
+    QAction * actionTogglePoi = dockPoi->toggleViewAction();
+    actionTogglePoi->setObjectName("actionTogglePoi");
+    actionTogglePoi->setIcon(QIcon(":/icons/32x32/TogglePoi.png"));
+    menuWindow->insertAction(actionSetupToolbar, actionTogglePoi);
 
     QAction * actionToggleWorkspace = dockWorkspace->toggleViewAction();
     actionToggleWorkspace->setObjectName("actionToggleWorkspace");
@@ -391,6 +402,7 @@ CMainWindow::CMainWindow()
                      << actionSetupGrid
                      << actionFlipMouseWheel
                      << actionSetupMapPaths
+                     << actionSetupPOIPaths
                      << actionPOIText
                      << actionNightDay
                      << actionMapToolTip
@@ -433,6 +445,7 @@ CMainWindow::CMainWindow()
                      << actionSetupToolbar
                      << actionToggleMaps
                      << actionToggleDem
+                     << actionTogglePoi
                      << actionToggleWorkspace
                      << actionToggleRealtime
                      << actionToggleDatabase
@@ -498,6 +511,7 @@ void CMainWindow::prepareMenuForMac()
     toolBar->toggleViewAction()->setMenuRole(QAction::NoRole);
     dockMaps->toggleViewAction()->setMenuRole(QAction::NoRole);
     dockDem->toggleViewAction()->setMenuRole(QAction::NoRole);
+    dockPoi->toggleViewAction()->setMenuRole(QAction::NoRole);
     dockWorkspace->toggleViewAction()->setMenuRole(QAction::NoRole);
     dockRealtime->toggleViewAction()->setMenuRole(QAction::NoRole);
     dockDatabase->toggleViewAction()->setMenuRole(QAction::NoRole);
@@ -576,6 +590,7 @@ CMainWindow::~CMainWindow()
     cfg.setValue("mapFont", mapFont);
     CMapDraw::saveMapPath(cfg);
     CDemDraw::saveDemPath(cfg);
+    CPoiDraw::savePoiPath(cfg);
     cfg.endGroup(); // Canvas
 
 
@@ -638,12 +653,12 @@ void CMainWindow::setupHomePath()
     }
 
     CMapDraw::setupMapPath(homeDir.absoluteFilePath(mapsPath));
+    CPoiDraw::setupPoiPath(homeDir.absoluteFilePath(poisPath));
     CDemDraw::setupDemPath(homeDir.absoluteFilePath(demPath));
     CRouterRoutino::self().setupPath(homeDir.absoluteFilePath(routinoPath));
     CRouterBRouter::self().setupLocalDir(homeDir.absoluteFilePath(brouterPath));
     cfg.setValue("Database/lastDatabasePath", homeDir.absoluteFilePath(databasePath));
     cfg.setValue("Paths/lastGisPath", homeDir.absoluteFilePath(gpxPath));
-
     cfg.setValue("Paths/homePath", homeDir.absolutePath());
 }
 
@@ -779,6 +794,11 @@ void CMainWindow::addMapList(CMapList * list, const QString &name)
 void CMainWindow::addDemList(CDemList * list, const QString &name)
 {
     tabDem->addTab(list, name);
+}
+
+void CMainWindow::addPoiList(CPoiList * list, const QString &name)
+{
+    tabPoi->addTab(list, name);
 }
 
 void CMainWindow::addWidgetToTab(QWidget * w)
@@ -1102,6 +1122,15 @@ void CMainWindow::slotCurrentTabCanvas(int i)
         }
     }
 
+    for(int n = 0; n < tabPoi->count(); n++)
+    {
+        if(compareNames(name, tabPoi->tabText(n)))
+        {
+            tabPoi->setCurrentIndex(n);
+            break;
+        }
+    }
+
     for(int n = 0; n < tabWidget->count(); n++)
     {
         CCanvas * canvas = dynamic_cast<CCanvas*>(tabWidget->widget(n));
@@ -1275,6 +1304,11 @@ void CMainWindow::slotSetupGrid()
 void CMainWindow::slotSetupMapPath()
 {
     CMapDraw::setupMapPath();
+}
+
+void CMainWindow::slotSetupPoiPath()
+{
+    CPoiDraw::setupPoiPath();
 }
 
 void CMainWindow::slotSetupDemPath()
@@ -1521,6 +1555,10 @@ void CMainWindow::slotLinkActivated(const QString& link)
     else if(link == "MapFolders")
     {
         slotSetupMapPath();
+    }
+    else if(link == "PoiFolders")
+    {
+        slotSetupPoiPath();
     }
     else if(link == "VrtBuilder")
     {

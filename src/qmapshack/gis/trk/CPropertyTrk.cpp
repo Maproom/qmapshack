199319f276c52448f240deaa452bef8ac5450d9d
Oliver Eichler <oliver.eichler@gmx.de>
2024-09-03 13:19:15 +0200

    Replace toTime_T by toSecsSinceEpoch

--- src/qmapshack/gis/trk/CPropertyTrk.cpp
+++ src/qmapshack/gis/trk/CPropertyTrk.cpp
@@ -55,7 +55,7 @@ void CPropertyTrk::setupData() {
     if ((key == CKnownExtension::internalProgress) || (key == CKnownExtension::internalSpeedTime)) {
       property.min = 0;
       property.axisType = property_t::eAxisTime;
-      property.getX = [](const CTrackData::trkpt_t& p) { return p.time.isValid() ? p.time.toTime_t() : NOFLOAT; };
+      property.getX = [](const CTrackData::trkpt_t& p) { return p.time.isValid() ? p.time.toSecsSinceEpoch() : NOFLOAT; };
     }
 
     if (key == CKnownExtension::internalSpeedDist) {

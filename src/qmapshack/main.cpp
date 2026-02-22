d19ba393d99f95b77fb5269f756ea05cafff1ed5
JFritzle <jfbeck@t-online.de>
2026-02-22 23:31:25 +0100

    [QMS-987] Fix: Menu icons on macOS missing

--- src/qmapshack/main.cpp
+++ src/qmapshack/main.cpp
@@ -40,6 +40,7 @@ int main(int argc, char** argv) {
   QCoreApplication::setOrganizationName("QLandkarte");
   QCoreApplication::setOrganizationDomain("qlandkarte.org");
   QCoreApplication::setApplicationVersion(VER_STR);
+  QCoreApplication::setAttribute(Qt::AA_DontShowIconsInMenus, false);
 
   IAppSetup* env = IAppSetup::getPlatformInstance();
   env->processArguments();

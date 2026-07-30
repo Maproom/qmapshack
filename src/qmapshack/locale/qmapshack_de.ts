102869cc88548c028c085eafd8f1f23c85e0b174
Robert Siebeck <robert@siebeck.org>
2026-07-30 10:30:50 +0200

    [QMS-1166] translate German labels

--- src/qmapshack/locale/qmapshack_de.ts
+++ src/qmapshack/locale/qmapshack_de.ts
@@ -8495,12 +8495,12 @@ Abgebrochen durch den Benutzer.
     <message>
         <location filename="../helpers/CShortcutSetupDialog.cpp" line="97"/>
         <source>Shortcut already in use</source>
-        <translation type="unfinished"></translation>
+        <translation>Der Kurzbefehl wird bereits benutzt</translation>
     </message>
     <message>
         <location filename="../helpers/CShortcutSetupDialog.cpp" line="98"/>
         <source>The shortcut &apos;%1&apos; is already assigned to &apos;%2&apos;. Do you want to reassign it to &apos;%3&apos;?</source>
-        <translation type="unfinished"></translation>
+        <translation>Der Kurzbefehl &apos;%1&apos; wird bereits für &apos;%2&apos; benutzt. Möchten Sie den Kurzbefehl auf &apos;%3&apos; ändern?</translation>
     </message>
 </context>
 <context>
@@ -15823,12 +15823,12 @@ Dateiname: %1</translation>
     <message>
         <location filename="../IMainWindow.ui" line="848"/>
         <source>Setup Keyboard Shortcuts</source>
-        <translation type="unfinished"></translation>
+        <translation>Tastaturkurzbefehle einrichten</translation>
     </message>
     <message>
         <location filename="../IMainWindow.ui" line="851"/>
         <source>Assign keyboard shortcuts to actions</source>
-        <translation type="unfinished"></translation>
+        <translation>Tastaturkurzbefehle zu Aktionen zuweisen</translation>
     </message>
     <message>
         <location filename="../IMainWindow.ui" line="869"/>
@@ -20306,32 +20306,32 @@ Wenn Sie das Wort &apos;wort&apos; eingeben, wird exakt nach dem Wort gesucht. W
     <message>
         <location filename="../helpers/IShortcutSetupDialog.ui" line="14"/>
         <source>Setup Keyboard Shortcuts</source>
-        <translation type="unfinished"></translation>
+        <translation>Tastaturkurzbefehle einrichten</translation>
     </message>
     <message>
         <location filename="../helpers/IShortcutSetupDialog.ui" line="20"/>
         <source>Filter actions...</source>
-        <translation type="unfinished"></translation>
+        <translation>Aktionen filtern...</translation>
     </message>
     <message>
         <location filename="../helpers/IShortcutSetupDialog.ui" line="40"/>
         <source>Action</source>
-        <translation type="unfinished"></translation>
+        <translation>Aktion</translation>
     </message>
     <message>
         <location filename="../helpers/IShortcutSetupDialog.ui" line="45"/>
         <source>Shortcut</source>
-        <translation type="unfinished"></translation>
+        <translation>Kurzbefehl</translation>
     </message>
     <message>
         <location filename="../helpers/IShortcutSetupDialog.ui" line="55"/>
         <source>Shortcut:</source>
-        <translation type="unfinished"></translation>
+        <translation>Kurzbefehl:</translation>
     </message>
     <message>
         <location filename="../helpers/IShortcutSetupDialog.ui" line="65"/>
         <source>Reset to Default</source>
-        <translation type="unfinished"></translation>
+        <translation>Voreinstellung</translation>
     </message>
 </context>
 <context>
